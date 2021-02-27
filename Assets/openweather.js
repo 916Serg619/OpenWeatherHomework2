150cdd6d08cd6b8e76c24ea3ccd8026641475983
Sergio Aguirre <sergioaguirre@sergios-mbp.attlocal.net>
2021-02-26 22:30:04 -0800

    Updated assignment, current city + 5-day forecast appear

--- Assets/openweather.js
+++ Assets/openweather.js
@@ -1,57 +1,174 @@
-//My personal APIKey//
-var APIKey = "295bbe3ca74a16499159d4b26aed24cf";
 
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
 
-//UV Index APIKey//
-var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=" + APIKey;
+var eCity=[];
 
-//This function will retirieve input data//
-$("#run-search").on("click", function() {
-
-    var city = $("#search-term").val().trim();
-    console.log(city);
-
-    weather(city);
-});
-//Function that retrieves weather data for the current city that was searched//
-function weather(city) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    APIKey;
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-
-        fiveDayWeather(city);
-
-        //Allows data like the current date, city name, temperature, and wind speed to display//
-        var a = new Date(response.dt * 1000).toLocaleDateString();
-        console.log(a);
-        $("#city-name-date").text(response.name + " " + d);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-
-
-        //Converts temperature from Kelvin to Farenheit//
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-        $(".temperature").text("Temperature (F) " + tempF.toFixed(2));
-    });
-};
-//Function to retrieve 5-day forecast data//
-function fiveDayWeather(city) {
-    var foreCastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid" + APIKey;
-    console.log(foreCastURL);
-
-    $.ajax({
-        url: foreCastURL,
-        method: "GET",
-    }).then(function(response) {
-        console.log(response);
-
-        var listArr = [respose.list[0], response.list[8], response.list[16], response.list[24], response.list[32]];
-        console.log(listArr);
-    })
+// searches city to see if it exists in storage entries
+function find(e){
+    for (var i=0; i<eCity.length; i++){
+        if(e.toUpperCase()===eCity[i]){
+            return -1;
+        }
+    }
+    return 1;
 }
+//Set up API key
+var APIKey="295bbe3ca74a16499159d4b26aed24cf";
+// Display the current and future weather 
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// Create ajax call to retrieve data
+function currentWeather(city){
+    // Build URL to get data from server (search by "city")
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        // parse the response to display the current weather including the City name, the date, and weather icon. 
+        console.log(response);
+        //Dta object from server side Api for icon property.
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        
+        // Format date
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        
+        //parse the response for name of city, concantenate date and icon
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        
+        // Display current temperature
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        // Convert the temp to fahrenheit
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+        // Display the Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+      
+        //Display wind speed and convert to MPH
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+       
+        // Display UVIndex.
+        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            eCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(eCity);
+            if (eCity==null){
+                eCity=[];
+                eCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(eCity));
+                cityList(city);
+            }
+            else {
+                if(find(city)>0){
+                    eCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(eCity));
+                    cityList(city);
+                }
+            }
+        }
+
+    });
+}
+    // This function returns the UVIindex response.
+function UVIndex(ln,lt){
+    //lets build the url for uvindex.
+    var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
+    $.ajax({
+            url:uvqURL,
+            method:"GET"
+            }).then(function(response){
+                $(currentUvindex).html(response.value);
+            });
+}
+    
+// Here we display the 5 days forecast for the current city.
+function forecast(cityid){
+    var dayover= false;
+    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fTemp"+i).html(tempF+"&#8457");
+            $("#fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
+
+//Add city to list of
+function cityList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+// display the past search again when the list group item is clicked in search history
+function invokePastSearch(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
+    }
+
+}
+
+// render function
+function loadlastCity(){
+    $("ul").empty();
+    var eCity = JSON.parse(localStorage.getItem("cityname"));
+    if(eCity!==null){
+        eCity=JSON.parse(localStorage.getItem("cityname"));
+        for(i=0; i<eCity.length;i++){
+            cityList(eCity[i]);
+        }
+        city=eCity[i-1];
+        currentWeather(city);
+    }
+
+}
+//Clear City seach history
+function clearHistory(event){
+    event.preventDefault();
+    eCity=[];
+    localStorage.removeItem("cityname");
+    document.location.reload();
+
+}
+//Click Handlers
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-For this assignment, I had to create an application that displays weather data, based on the user's search.  The goal is to have the user inut a particular city, and have that city's current wether information display, as well as the five-day forecast for that city.  The user has the option to search multiple cities, with a list appearing of cities already viewed.  
-
-
-
-I acquired my own API key for this project.  Using that, as well as several APIs referenced from openweathermap.org, I attempted to retireve the data with a couple functions.  Now, from my understanding, evrything I did was "right" (I worked with a classmate), but NOTHING was retrieved, or even logged in the console. When I went to inspect, no issues were reported, so there was no hint, or guidance to where my problem was coming from.  This basicall left me at a stand-still for the rest of the assignment. 
+For this assignment, I had to create an application that displays weather data, based on the user's search. The goal is to have the user inut a particular city, and have that city's current wether information display, as well as the five-day forecast for that city. The user has the option to search multiple cities, with a list appearing of cities already viewed.
 
 ## Areas to Improve
 
-I was successful in console logging the information needed BEFORE I created my functions, so I know that at one point, I was getting somewhere.  after I added the code that was necessary to retrieve the desired data, the code stopped being responsive (in the inspector, as well as the console itself). Still need improvement working with localStorage, but I am more curious as to why I could not see any results.  I would have been more happy seeing mulitple errors pop up.  At least I would have an idea of where to start de-bugging. 
+I was successful in console logging the information needed BEFORE I created my functions, so I know that at one point, I was getting somewhere. after I added the code that was necessary to retrieve the desired data, the code stopped being responsive (in the inspector, as well as the console itself). Still need improvement working with localStorage, but I am more curious as to why I could not see any results. I would have been more happy seeing mulitple errors pop up. At least I would have an idea of where to start de-bugging.
+
+UPDATE: Successful in retreiving current city data, 5-day forecast, and clearing search history. For the future, I want to specify the exact state for more specific results.
+
+## LICENSE
+
+MIT
 
 ## Authors
 
